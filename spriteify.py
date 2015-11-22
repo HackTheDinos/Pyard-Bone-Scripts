1ef4bca162d36d2e482d472d7a55f34208762f5e
Dean Silfen <dean.silfen@gmail.com>
2015-11-22 02:47:52 -0500

    Start to write sprite files
    However I am getting segfaults after writing one sprite, so something
    must be wrong

--- spriteify.py
+++ spriteify.py
@@ -14,6 +14,26 @@ MAX_FILE_COUNT = 50
 ### PROCESSING THE DATA
 def new_name(old_name):
     old_name = old_name.split('.')[0]
+    return 'tmp_sprite_' + old_name + '.png'
+
+def clean_tiff(path, tiff):
+    new_tiff_path = os.path.join(path, tiff)
+    try:
+        exif_dict = piexif.load(new_tiff_path)
+        if not exif_dict.get('ImageDescription'):
+            exif_dict['ImageDescription'] = 'This is not null'
+    except ValueError:
+        print('File {} is borked'.format(new_tiff_path))
+        exif_dict = {} # TODO: generate exif data? I guess?
+        return
+
+    new_tiff = Image.open(new_tiff_path)
+    new_tiff.save(new_tiff_path, exif=exif_dict)
+    new_png_name = new_name(tiff)
+    new_png_path = os.path.join(path, new_png_name)
+    call('convert {0} --type Grayscale {1}'.format(new_tiff_path, new_png_path))
+    return new_png_path
+
 ### GETTING THE DATA
 def process_files(dir_path, filenames):
     total_files = len(filenames)
@@ -22,7 +42,11 @@ def process_files(dir_path, filenames):
 
     step = ceil(total_files / MAX_FILE_COUNT)
     chosen_files = filenames[::step]
-    test_img = Image.open(chosen_files)
+    try:
+        test_path = os.path.join(dir_path, chosen_files[0])
+        test_img = Image.open(test_path)
+    except IndexError:
+        return False
 
     width = test_img.width * len(chosen_files)
     height = test_img.height
@@ -31,6 +55,8 @@ def process_files(dir_path, filenames):
 
     for filename in chosen_files:
         png_path = clean_tiff(dir_path, filename)
+        if not png_path:
+            continue
         png = Image.open(png_path)
         blank_image.paste(png, paste_cords)
         x, y = paste_cords
