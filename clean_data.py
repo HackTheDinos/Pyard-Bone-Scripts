d4fce21566850f288b75392e5765041618ffb084
Dean Silfen <dean.silfen@gmail.com>
2015-11-22 02:30:37 -0500

    add status.txt to cleaned data

--- clean_data.py
+++ clean_data.py
@@ -1,9 +1,10 @@
 import os
 
+from json import dumps
 from uuid import uuid4
 from shutil import copy2
 from sys import argv
-
+from time import time
 # takes argument of the root dir of the data:
 # IE: python clean_data.py /Users/deansilfen/HTD/CTdata_copy
 # takes around 10 seconds to run on ~15GB of data
@@ -16,15 +17,27 @@ def make_unless_exists(path):
 
 def is_eligable_file(f):
     f = f.lower()
-    return f.endswith('tif') or f.endswith('tiff') or f.endswith('stl')
+    return f.endswith('tif') or f.endswith('tiff') or f.endswith('stl') or f.endswith('pca')
+
+def status(root, **kwargs):
+    filepath = os.path.join(root, 'status.txt')
+    with open(filepath, 'w') as f:
+        f.write(dumps(kwargs))
 
 def walk_and_clean(walking_root):
-    walkable = os.walk(root)
     ROOT = str(uuid4())
-    new_root = os.path.join(root, ROOT)
+    files_written = 0
+    new_root = os.path.join(walking_root, ROOT)
     os.mkdir(new_root)
-    root_depth = len(root.split('/'))
 
+    status(new_root,
+        timestamp=int(time()),
+        state='initializing',
+        files_written=files_written
+    )
+
+    walkable = os.walk(walking_root)
+    root_depth = len(root.split('/'))
     for dir_path, dir_names, filenames in walkable:
         if ROOT in dir_path:
             continue
@@ -40,8 +53,8 @@ def walk_and_clean(walking_root):
         specimen_path = os.path.join(new_root, specimen_name)
         make_unless_exists(specimen_path)
 
-        if all([filename.lower().endswith('stl') for filename in movable_files]):
-            curr_dir = 'STL_data'
+        if all([filename.lower().endswith('stl') or filename.lower().endswith('pca') for filename in movable_files]):
+            curr_dir = 'extra_data'
         else:
             curr_dir = os.path.dirname(dir_path).split('/')[-1]
 
@@ -56,6 +69,19 @@ def walk_and_clean(walking_root):
             old_files_path = os.path.join(dir_path, files)
             new_files_path = os.path.join(new_path, files)
             copy2(old_files_path, new_files_path)
+            files_written += 1
+            status(new_root,
+                timestamp=int(time()),
+                state='processing',
+                files_written=files_written
+            )
+
+    status(new_root,
+        timestamp=int(time()),
+        state='finished',
+        files_written=files_written
+    )
+
 
 
 if __name__ == '__main__':
